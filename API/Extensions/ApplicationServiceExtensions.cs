e27b9b81104d869a9fb10e55c0022d4c4a089f6c
vijaybabar <babarvm@gmail.com>
2024-07-20 18:46:35 +0530

    Section 3 Extension methods DRY

---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,25 @@
+﻿using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions;
+
+public static class ApplicationServiceExtensions
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddControllers();
+        // Register database
+        services.AddDbContext<DataContext>(opt => 
+        {
+            opt.UseSqlite(config.GetConnectionString("DefaultCS"));
+        });
+        // Add Cors
+        services.AddCors();
+        // Add Services
+        services.AddScoped<ITokenService, TokenService>();
+
+        return services;
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,36 +1,9 @@
-using System.Text;
-using API.Data;
-using API.Interfaces;
-using API.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
+using API.Extensions;
 
 // Add services to the container.
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllers();
-// Register database
-builder.Services.AddDbContext<DataContext>(opt => 
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultCS"));
-});
-// Add Cors
-builder.Services.AddCors();
-// Add Services
-builder.Services.AddScoped<ITokenService, TokenService>();
-// Add Authentication
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(opt => 
-    {
-        var tokenKey = builder.Configuration["TokenKey"] ?? throw new Exception("Token key not found");
-        opt.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey)),
-            ValidateIssuer = false,
-            ValidateAudience = false
-        };
-    });
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 
 
 // Configure the HTTP request pipeline.
